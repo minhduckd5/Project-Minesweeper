ccb4164a244e8edef9791d7ca4c1b2e20ae5173c
Duke <93232187+minhduckd5@users.noreply.github.com>
2024-05-02 22:02:23 +0700

    add libraries

---
+++ UI.java
@@ -0,0 +1,12 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class UI extends JPanel{
+    
+}
