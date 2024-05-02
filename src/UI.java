49b202fae4856c451c8d3c5313d511464576922c
Duke <93232187+minhduckd5@users.noreply.github.com>
2024-05-02 22:11:31 +0700

    Initiate variables + images

--- src/UI.java
+++ src/UI.java
@@ -9,5 +9,26 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 public class UI extends JPanel{
-    
+    private final int NUM_IMAGES = 13;
+    private final int CELL_SIZE = 60;
+
+    private final int COVER_FOR_CELL = 10;
+    private final int MARK_FOR_CELL = 10;
+    private final int EMPTY_CELL = 0;
+    private final int MINE_CELL = 9;
+    private final int COVERED_MINE_CELL = MINE_CELL + COVER_FOR_CELL;
+    private final int MARKED_MINE_CELL = COVERED_MINE_CELL + MARK_FOR_CELL;
+
+    private final int DRAW_MINE = 9;
+    private final int DRAW_COVER = 10;
+    private final int DRAW_MARK = 11; 
+    private final int DRAW_WRONG_MARK = 12;
+
+    private int N_MINES = 40;
+    private int N_ROWS = 16;
+    private int N_COLS = 16;
+
+    private final int BOARD_WIDTH = N_COLS * CELL_SIZE + 1;
+    private final int BOARD_HEIGHT = N_ROWS * CELL_SIZE + 1;
+
 }
